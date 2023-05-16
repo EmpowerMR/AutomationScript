48ffe1a38bdd83cfb06ae80405cf93cb14ce09fc
TS Recorder Two <TS Recorder Two@LAPTOP-B14439RR>
2023-05-16 14:09:30 -0400

    Add project files.

---
+++ HelloWorld_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorld_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWorld_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F2A8BC9C-BF64-4351-B541-ABF0A457EB51")]
+[assembly: AssemblyVersion("1.0.0.0")]
